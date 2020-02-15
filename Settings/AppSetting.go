79a33709cf920db11a06c0df3ac7499ec1d26058
Evan.Lyu <>
2020-02-16 05:17:02 +0800

    app rpc port can't typed uint with env loader

--- Settings/AppSetting.go
+++ Settings/AppSetting.go
@@ -4,6 +4,6 @@ type AppConf struct {
 	Codename       string `envField:"app:Codename" default:""`
 	LogLevel       string `envField:"app:LogLevel" default:"info"`
 	RpcCommandMode bool   `envField:"app:RpcCommandMode" default:"false"`
-	RpcBindPort    uint   `envField:"app:RpcBindPort" default:"9999"`
+	RpcBindPort    int    `envField:"app:RpcBindPort" default:"9999"`
 	RpcEndpoint    string `envField:"app:RpcEndpoint" default:"0.0.0.0"`
 }
