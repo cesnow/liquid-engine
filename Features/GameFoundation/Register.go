e1adafa85731e0abce42d09982e84af6fa58ec2d
evan <b790113g@gmail.com>
2024-02-22 15:24:09 +0800

    feat: register with http status

--- Features/GameFoundation/Register.go
+++ Features/GameFoundation/Register.go
@@ -18,34 +18,35 @@ func RouteRegister(c *gin.Context) {
 	if command.FromType == "" {
 		Logger.SysLog.Errorf("[CMD][Register] Create Member Failed, From Type is empty")
 		result := gin.H{"registerStatus": 0, "error": "from_type is empty"}
-		c.String(http.StatusOK, Middlewares.GetLiquidResult(result))
+		c.String(http.StatusBadRequest, Middlewares.GetLiquidResult(result))
 		c.Abort()
 		return
 	}
 
-	if command.Account == "" || command.Password == ""{
+	if command.Account == "" || command.Password == "" {
 		Logger.SysLog.Errorf("[CMD][Register] Create Member Failed, Account/Password is empty")
 		result := gin.H{"registerStatus": 0, "error": "account/password is empty"}
-		c.String(http.StatusOK, Middlewares.GetLiquidResult(result))
+		c.String(http.StatusBadRequest, Middlewares.GetLiquidResult(result))
 		c.Abort()
 		return
 	}
 
 	member := LiquidSDK.GetServer().GetMemberSystem(command.FromType)
-	errorMessage := ""
-	resultStatus := 0
 	if member == nil {
-		errorMessage = "member system is not defined : " + command.FromType
-	} else {
-		resultStatus, errorMessage = member.Register(
-			command.FromType,
-			command.Account,
-			command.Password,
-			"",
-			command.ExtraData,
-		)
+		c.String(http.StatusForbidden, Middlewares.GetLiquidResult(gin.H{
+			"registerStatus": 0,
+			"error":          "member system is not defined : " + command.FromType,
+		}))
+		c.Abort()
 	}
 
+	resultStatus, errorMessage := member.Register(
+		command.FromType,
+		command.Account,
+		command.Password,
+		"",
+		command.ExtraData,
+	)
 	result := gin.H{"registerStatus": resultStatus, "error": errorMessage}
 	c.String(http.StatusOK, Middlewares.GetLiquidResult(result))
 }
